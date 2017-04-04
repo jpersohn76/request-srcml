f4bad28f5dff007f60908cd12754a563591e79d1
jpersohn76 <azuser@cs480student22.fkxrf4k0zaee5nzuxeei5lyjah.bx.internal.cloudapp.net>
2017-04-04 21:17:55 +0000

    last commit didn't work properly

--- request_srcml.cpp
+++ request_srcml.cpp
@@ -58,17 +58,22 @@ std::string request_language(const srcml_request& request, const std::string& fi
 }
 
 // generates srcML based on the request
-void generate_srcml(const srcml_request& request) {
+bool generate_srcml(const srcml_request& request) {
 
 	std::string filename = request_filename(request);
 	
-	// if (filename == "-")
-		// std::cout << "Using stdin requires a declared language" << "\n";
-	
+	if (filename == "-") {
+		std::err << "Using stdin requires a declared language" << "\n";
+		return false;
+	}
 	std::string language = request_language(request, filename);
 	
-	// if (language == "")
-		// std::cout << "Extension not supported" << "\n";
-
+	if (language == "") {
+		std::err << "Extension not supported" << "\n";
+		return false;
+	}
+	
+	return true;
+	
 	// srcML generation code that is not available yet
 }
